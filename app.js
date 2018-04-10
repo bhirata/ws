a5d9623823a2ab651b03ea8d4d1154181b655ffc
Bruno Hirata <hiratabruno@yahoo.com>
2018-04-10 18:21:49 -0300

    First

---
+++ app.js
@@ -0,0 +1,68 @@
+var path = require('path');
+var app = require('express')();
+var WebSocket = require('ws');
+
+
+const ws = new WebSocket('ws://localhost:3002');
+
+ws.onopen = function () {
+  console.log("Connected to WebSocket Server");
+};
+ws.onclose = function (){
+  console.log("Desconnected from WebSocket Server");
+};
+
+ws.onmessage = function (payload) {
+  console.log("Message received from Server: " + payload.data);
+};
+
+app.get('/auth', (req, res) => {
+  let authObj = {
+    "event" : "auth",
+    "command" : {
+      "token" : "123128371623="
+    }
+  };
+
+  console.log("Express connection");
+  ws.on('auth', () => {
+    ws.send(authObj);
+  });
+});
+
+/*
+(function(){
+  ws.on('message', () => {
+    ws.send("X");
+  });
+}) ();
+*/
+
+app.listen(3000, () => console.log("listening on http://localhost:3000"));
+
+
+/*
+Após estabelecer a conexão com o websocket, deve-se enviar o evento de autenticação:
+
+{
+
+  "event":"auth",
+
+    "command":{
+
+  "token":"123128371623="
+
+}
+
+}
+
+Retorno:
+
+{
+
+  "event":"auth",
+
+    "success":true
+
+}
+ */
